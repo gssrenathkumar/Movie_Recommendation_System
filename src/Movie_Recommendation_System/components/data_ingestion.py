26414b0b58f7bc921a93cfb5fc2bee21eabb6b62
gssrenathkumar <srenathindustries215@gmail.com>
2023-06-15 15:30:43 +0530

    Data Validation is completed and ready for data transformation

--- src/Movie_Recommendation_System/components/data_ingestion.py
+++ src/Movie_Recommendation_System/components/data_ingestion.py
@@ -27,7 +27,7 @@ class DataIngestion:
         Extracts the zip file into the data directory
         Function returns None
         """
-        unzip_path = self.config.unzip_dir
+        unzip_path = os.path.join(self.config.unzip_dir, 'dataset')
         os.makedirs(unzip_path, exist_ok=True)
         with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
             zip_ref.extractall(unzip_path)
